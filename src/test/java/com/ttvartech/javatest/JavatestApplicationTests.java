74314aa453b206439040c687fc2035dd603efe00
aleksandra <aleksandra.vujadinovic@elfak.rs>
2023-07-21 21:03:50 +0200

    initial commit

---
+++ src/test/java/com/ttvartech/javatest/JavatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttvartech.javatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
